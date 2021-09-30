41a2941c0537755bd4012f5e75da68208ac9d426
root <root@localhost>
2021-09-30 12:15:40 +0000

    Base Application

---
+++ spec/feature/integration_spec.rb
@@ -0,0 +1,23 @@
+
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+  end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Price', with: 14.99
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+  end
+end
